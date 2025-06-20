9b130ab60ee9eb1a8794a6774395b96c81c4d37c
Pavel <Pav_kaf@mail.ru>
2025-06-21 02:02:30 +0300

    remove log

--- src/core/storage/index.js
+++ src/core/storage/index.js
@@ -8,7 +8,6 @@ export async function setValueInStorage(key, value) {
 
 export async function initializeOption(option) {
   let result = await getValueFromStorage(option.id);
-  console.log(result, "res");
   if (result[option.id] === undefined) {
     await setValueInStorage(option.id, option.value);
     return option.value;
