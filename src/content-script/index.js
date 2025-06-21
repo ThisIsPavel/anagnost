3513310da4fe38c1e87240cdf749ad2521808849
Pavel <Pav_kaf@mail.ru>
2025-06-21 18:12:41 +0300

    feat: adds handlers for block reading

--- src/content-script/index.js
+++ src/content-script/index.js
@@ -1,3 +1,5 @@
+import { DEFAULT_READING_MODE } from "../constant/defaultValue";
+import { getValueFromStorage } from "../core/storage";
 import {
   getTextContent,
   processChildren,
@@ -6,14 +8,56 @@ import {
 import SPEECH_RULES from "./speech-engine/rules.config";
 import { speak } from "./speech-engine/tts";
 
-chrome.runtime.onMessage.addListener((request) => {
+let isAltPressed = false;
+
+chrome.runtime.onMessage.addListener(async (request) => {
   console.log("asd");
   if (request.action === "read") {
     processElement(document.body);
   } else if (request.action === "stop") {
     window.speechSynthesis.cancel();
+  } else if (request.action === "switchReadingMode") {
+    const { checkbox: isBlockReadingMode } = await getValueFromStorage(
+      DEFAULT_READING_MODE.id
+    );
+    document.removeEventListener("keydown", handleAltKey, true);
+    document.removeEventListener("keyup", handleAltKey, true);
+    document.removeEventListener("click", handleClickWithAlt, true);
+    document.body.removeEventListener("mouseover", handleMouseOver, true);
+    document.body.removeEventListener("mouseout", handleMouseOut, true);
+
+    if (isBlockReadingMode) {
+      document.body.addEventListener("mouseover", handleMouseOver, true);
+      document.body.addEventListener("mouseout", handleMouseOut, true);
+    }
   }
 });
+// TODO: refactor handlers
+function handleAltKey(e) {
+  isAltPressed = e.type === "keydown" && e.key === "Alt";
+}
+
+function handleClickWithAlt(e) {
+  if (isAltPressed && e.button === 0) {
+    e.preventDefault();
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+    processElement(e.target);
+  }
+}
+
+function handleMouseOver(e) {
+  const element = e.target;
+  element.style.outline = "2px solid #000";
+  document.addEventListener("keydown", handleAltKey, true);
+  document.addEventListener("keyup", handleAltKey, true);
+  document.addEventListener("click", handleClickWithAlt, true);
+}
+
+function handleMouseOut(e) {
+  const element = e.target;
+  element.style.outline = "";
+}
 
 export function processElement(element) {
   if (shouldSkip(element)) return;
